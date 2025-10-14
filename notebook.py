d7d6f17fe87209734bb5d06c49f0e6676d057079
MadeInShineA <olivier.amacker@netplus.ch>
2025-10-14 16:21:45 +0200

    Created notebook

---
+++ notebook.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.16.5"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# Hello world""")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
