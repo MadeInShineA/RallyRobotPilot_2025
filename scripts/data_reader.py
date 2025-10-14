aabcd26ad7e0781b17963c6b878e70b0cfb1cc8e
MadeInShineA <olivier.amacker@netplus.ch>
2025-10-14 16:17:49 +0200

    Changed the data_collector record save directory

--- scripts/data_reader.py
+++ scripts/data_reader.py
@@ -1,14 +1,11 @@
-
-
 if __name__ == "__main__":
     import numpy
     import pickle
     import lzma
 
-
-    with lzma.open("record_0.npz", "rb") as file:
+    with lzma.open("record_1.npz", "rb") as file:
         data = pickle.load(file)
 
         print("Read", len(data), "snapshotwas")
-        print(data[0].image)
-        print([e.current_controls for e in data])
+        for data_point in data:
+            print(f"Angle: {data_point.car_angle}")
